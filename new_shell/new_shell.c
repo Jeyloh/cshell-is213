f04edcaf6aa66520fa62a900f17f86390def2e3a
sifusan <fuglestad.simen@gmail.com>
2016-04-23 18:03:14 +0200

    added separate function for handling command execution

--- new_shell/new_shell.c
+++ new_shell/new_shell.c
@@ -2,13 +2,40 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <memory.h>
-//Simple beginnings of a shell
+
+//Declare the functions we want to use as string parameteres
+//NOTE: builtin functions like "cd" and "exit" is not yet supported
+char *functions[] = {
+        "ls\n",
+        "cd\n",
+        "clear\n",
+        "exit\n",
+        "help\n"
+    };
+    
+//Execute a command provided by the user
+void execute_cmd(char cmd[]) {
+    int i;
+    //Check the functions to determine which system call to perform
+    for (i = 0; i < sizeof(functions)/sizeof(functions[0]); i++) {
+        if(strcmp(cmd, functions[i]) == 0) {
+            system(cmd);
+            break;
+        }
+    }
+}
+
+#define MAX_DATA 1024
 int main() {
+	char line[MAX_DATA];
 	printf("Welcome to the new shell!\n");
-	char c[1024];
+	
 	while(1) {
-		fgets(c, 1024, stdin);
-		system(c);
+	    printf("$ ");
+	    //reads the user input and store it in line, will break if input is
+	    //greater than 1024 bytes
+		if (!fgets(line, 1024, stdin)) break;
+		execute_cmd(line);
 	}
 	return 0;
 }
