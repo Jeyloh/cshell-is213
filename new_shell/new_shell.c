c60d8b0da8ebe2f32a48d185d19dc425799d3090
sifusan <fuglestad.simen@gmail.com>
2016-04-24 13:37:58 +0200

    added cd

--- new_shell/new_shell.c
+++ new_shell/new_shell.c
@@ -3,31 +3,37 @@
 #include <stdlib.h>
 #include <memory.h>
 
-//Declare the functions we want to use as string parameteres
-//NOTE: builtin functions like "cd" and "exit" is not yet supported
-char *functions[] = {
-        "ls\n",
-        "cd\n",
-        "clear\n",
-        "exit\n",
-        "help\n"
-    };
-    
+//Get arguments from a supplied command, this is only necessary for special 
+//commands like 'cd' which cannot be called upon by system().
+char * getargs(char *cmd) {
+    char *args = (char *)malloc(strlen(cmd));
+    //Fill in processing here to filter out args
+    return args;
+}
+   
 //Execute a command provided by the user
-void execute_cmd(char cmd[]) {
-    int i;
-    //Check the functions to determine which system call to perform
-    for (i = 0; i < sizeof(functions)/sizeof(functions[0]); i++) {
-        if(strcmp(cmd, functions[i]) == 0) {
-            system(cmd);
-            break;
+void execute_cmd(char *cmd) {
+    char *args = getargs(cmd);
+    if (strcmp(cmd, "cd") == 0) {
+        if (!args || strlen(args) == 0) {
+            printf("Supplied no args to cd: usage is cd <path/to/dir>");
+        } else {
+            chdir(args);
         }
+    }
+    else if (strcmp(cmd, "exit") == 0) {
+        exit(0);
+    } else {
+        system(cmd);
     }
 }
 
 #define MAX_DATA 1024
 int main() {
 	char line[MAX_DATA];
+	char *cmd;
+	char *args;
+	int i;
 	printf("Welcome to the new shell!\n");
 	
 	while(1) {
@@ -35,7 +41,15 @@ int main() {
 	    //reads the user input and store it in line, will break if input is
 	    //greater than 1024 bytes
 		if (!fgets(line, 1024, stdin)) break;
-		execute_cmd(line);
+		cmd = (char *)malloc(strlen(line));
+		//Copy the content that is string from line to cmd, skipping '\n'
+		for (i = 0; i < strlen(line) - 1; i++) {
+		    cmd[i] = line[i];
+		}
+		
+		execute_cmd(cmd);
+		//system(line);
+		free(cmd);
 	}
 	return 0;
 }
